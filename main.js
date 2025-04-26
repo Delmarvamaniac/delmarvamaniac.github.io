0ecbd7bc7f05e79d61319b11733af4d98320a7e8
Eggrens <jaxaphone444@gmail.com>
2025-04-25 21:54:19 -0400

    added listing sort feature, including sort by date posted; added more house examples to json file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const onIndex = (window.location.pathname == "/index.html" || window.location.pathname == "/");    // true if page is index.html (homepage), false otherwise (meaning we are on saved.html)
 var listingsRow = onIndex ? document.getElementById("featuredListings") : document.getElementById("savedListings");
 var map = null;
+var listingSort = document.getElementById("listingSort");
 
 // Init Leaflet map if on index.html
 if (onIndex) {
@@ -11,37 +12,79 @@ if (onIndex) {
     }).addTo(map);
 }
 
-const houses = JSON.parse(localStorage.getItem("houses"));   // Fetch houses from local storage
-
-// Generate listing cards for each house and add them to the page
-for (let i = 0; i < houses.length; i++) {
-    const house = houses[i];
-    if (!(onIndex || house.saved)) continue;    // if on saved.html, skip house if it is not saved
-
-    // Make house card and add to listings row
-    const card = makeCard(house, i, false);
-    var cardDiv = document.createElement('div');
-    cardDiv.className = "col-md-4";
-    cardDiv.innerHTML = card;
-    listingsRow.appendChild(cardDiv);
-
-    // Add map markers for listings if on index.html
-    if (onIndex) {
-        const coords = house.coordinates;
-        const markerCard = makeCard(house, i, true);
-        var houseMarker = L.marker(coords).addTo(map);
-        houseMarker.bindPopup(markerCard);
-    }
+const priceLowHigh = (a, b) => {
+    return a.price - b.price;
 }
 
-// if on saved.html, show a message if no listings are currently saved
-if (!onIndex && listingsRow.children.length == 0) {
-    let div = document.createElement('div');
-    div.className = "mb-3 mt-3 text-center";
-    let h = document.createElement('h3');
-    h.innerText = "You have no saved listings."
-    div.appendChild(h);
-    listingsRow.appendChild(div);
+const priceHighLow = (a, b) => {
+    return b.price - a.price;
+}
+
+const dateRecent = (a, b) => {
+    return new Date(b.date) - new Date(a.date);
+}
+
+const dateOldest = (a, b) => {
+    return new Date(a.date) - new Date(b.date);
+}
+
+window.addEventListener("load", (event) => {
+    displayListings(listingSort.value);
+});
+
+listingSort.addEventListener("change", (event) => {
+    listingsRow.innerHTML = "";     // clear previous sorted content when changing sort
+    displayListings(event.target.value);
+});
+
+function displayListings(sort) {
+    var houses = JSON.parse(localStorage.getItem("houses"));   // Fetch houses from local storage
+
+    // Sort houses by selected sort order
+    switch (sort) {
+        case "1":   // sort by Recent
+            houses = houses.sort(dateRecent);
+            break;
+        case "2":   // sort by Oldest
+            houses = houses.sort(dateOldest);
+            break;
+        case "3":   // sort by Price (Low to High)
+            houses = houses.sort(priceLowHigh);
+            break;
+        default:    // otherwise, sort by Price (High to Low)
+            houses = houses.sort(priceHighLow);
+    }
+
+    // Generate listing cards for each house and add them to the page
+    for (let i = 0; i < houses.length; i++) {
+        const house = houses[i];
+        if (!(onIndex || house.saved)) continue;    // if on saved.html, skip house if it is not saved
+
+        // Make house card and add to listings row
+        const card = makeCard(house, house.id, false);
+        var cardDiv = document.createElement('div');
+        cardDiv.className = "col-md-4";
+        cardDiv.innerHTML = card;
+        listingsRow.appendChild(cardDiv);
+
+        // Add map markers for listings if on index.html
+        if (onIndex) {
+            const coords = house.coordinates;
+            const markerCard = makeCard(house, house.id, true);
+            var houseMarker = L.marker(coords).addTo(map);
+            houseMarker.bindPopup(markerCard);
+        }
+    }
+
+    // if on saved.html, show a message if no listings are currently saved
+    if (!onIndex && listingsRow.children.length == 0) {
+        let div = document.createElement('div');
+        div.className = "mb-3 mt-3 text-center";
+        let h = document.createElement('h3');
+        h.innerText = "You have no saved listings."
+        div.appendChild(h);
+        listingsRow.appendChild(div);
+    }
 }
 
 // Makes a listing card, either plain or for the map
@@ -52,12 +95,21 @@ function makeCard(house, i, marker) {
     const beds = house.beds;
     const baths = house.baths;
     const area = house.area + " " + house.areaUnits;
+    const date = house.date;
+    const forSale = house.forSale;
 
     return `
-    <div class="card">
-        <img src="${thumbnail}" class="card-img-top" alt="Property Image">
+    <div class="card mb-3">
+        <img src="${thumbnail}" class="card-img-top" style="width: 100%; ${!marker ? 'height: 15vw;' : 'height: 8vw;'} object-fit: cover;" alt="Property Image">
         <div class="card-body">
-        <h5 class="card-title">$${price}</h5>
+        <div class="row">
+            <div class="col">
+                <h5 class="card-title">$${forSale ? price : price + "/month"}</h5>
+            </div>
+            <div class="col text-end">
+                <p><i class="fa fa-calendar"></i> ${date}</p>
+            </div>
+        </div>
         <p><i class="fa fa-map-marker"></i> ${address}</p>
         <div class="row">
             <div class="col-4 text-center">
